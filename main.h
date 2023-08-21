09de455df14adb8b08082245f23237037fbee050
Lawrence Appiah-Nuamah <lappiahnumahfbc1999@gmail.com>
2023-08-21 03:42:30 -0700

    Adding main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* This a header file containing declarations
+ * for prototype functions defined in the printf project
+ * tasks and directory
+ * The tasks defines  the functions
+ */
+int _printf(const char *format, ...);
+#endif
