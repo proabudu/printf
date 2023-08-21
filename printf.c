8aeedd068191f84a62b6499e6d75c49858af44b2
proabudu <abuduprosperyao@gmail.com>
2023-08-21 12:15:40 -0700

    printf.c

---
+++ printf.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _printf - prints output according to a format.
+* @format: character string.
+*
+* Return: the number of characters printed (excluding the null byte used to end output to strings)
+*/
+int _printf(const char *format, ...)
+{
+int i = 0, count = 0;
+va_list args;
+va_start(args, format);
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == 'c')
+{
+char c = (char) va_arg(args, int);
+count += write(1, &c, 1);
+}
+else if (format[i] == 's')
+{
+char *s = va_arg(args, char *);
+count += write(1, s, strlen(s));
+}
+else if (format[i] == '%')
+{
+count += write(1, "%", 1);
+}
+}
+else
+{
+count += write(1, &format[i], 1);
+}
+i++;
+}
+va_end(args);
+return count;
+}
+
