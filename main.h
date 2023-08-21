7158a6de2c83d6898db7e2dcfaf66e6f035b44fe
proabudu <abuduprosperyao@gmail.com>
2023-08-21 12:27:04 -0700

    .gitignore file created

--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-/* This a header file containing declarations
- * for prototype functions defined in the printf project
- * tasks and directory
- * The tasks defines  the functions
- */
-int _printf(const char *format, ...);
-#endif
