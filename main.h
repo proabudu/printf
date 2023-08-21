49fdd79ef3c4da39d8acb7526dad9605ca475c97
proabudu <abuduprosperyao@gmail.com>
2023-08-21 15:02:38 -0700

    trial

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
+main.h
+
 #ifndef MAIN_H_
 #define MAIN_H_
 
 int _printf(const char *format, ...);
+int main(void);
 
 #endif // MAIN_H_
 
--- printf.c
+++ printf.c
@@ -3,7 +3,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
-#include "main.h"
+#include "printf.h"
 
 /**
 * _printf - prints output according to a format.
