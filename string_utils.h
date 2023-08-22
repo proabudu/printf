42c56796e8121c7e102df183313b6963a724979d
proabudu <abuduprosperyao@gmail.com>
2023-08-22 17:38:49 +0000

    STRING_UTILS_H

---
+++ string_utils.h
@@ -0,0 +1,11 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+void _str_reverse(char *str);
+char *_itoa(int num, char *str);
+int _strlen(const char *str);
+char *_utoa(unsigned int num, char *str);
+char *_utoa_base(unsigned int num, char *str, int base, int uppercase);
+
+#endif /* STRING_UTILS_H */
+
