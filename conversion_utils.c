5a0d8851c8d9300c14d3381d774722d876d5c6da
proabudu <abuduprosperyao@gmail.com>
2023-08-22 21:11:07 +0000

    utils

--- conversion_utils.c
+++ conversion_utils.c
@@ -1,64 +1,16 @@
-#include <unistd.h>
-#include "conversion_utils.h"
-
-/**
- * print_octal - Print an unsigned integer in octal
- * @n: Unsigned integer to print
- * Return: Number of characters printed
- */
-int print_octal(unsigned int n)
-{
-    char octal[12];
-    int count = 0, i = 0;
-
-    if (n == 0)
-    {
-        write(1, "0", 1);
-        return (1);
-    }
-
-    while (n > 0)
-    {
-        octal[i] = (n % 8) + '0';
-        n /= 8;
-        i++;
-    }
-
-    while (i > 0)
-    {
-        write(1, &octal[i - 1], 1);
-        count++;
-        i--;
-    }
-
-    return count;
-}
+#include <stdio.h>
+#include "main.h"
 
 /**
  * print_hexadecimal - Print an unsigned integer in hexadecimal
  * @n: Unsigned integer to print
- * @uppercase: Whether to use uppercase letters for A-F (1) or not (0)
+ * @uppercase: Whether to use uppercase letters (1) or not (0)
  * Return: Number of characters printed
  */
 int print_hexadecimal(unsigned int n, int uppercase)
 {
-    char hex[16];
-    int count = 0, i = 0;
-
-    while (i < 10)
-    {
-        hex[i] = i + '0';
-        i++;
-    }
-
-    while (i < 16)
-    {
-        hex[i] = (uppercase ? 'A' : 'a') + (i - 10);
-        i++;
-    }
-
     char hexadecimal[12];
-    i = 0;
+    int count = 0, i = 0;
 
     if (n == 0)
     {
@@ -68,7 +20,8 @@ int print_hexadecimal(unsigned int n, int uppercase)
 
     while (n > 0)
     {
-        hexadecimal[i] = hex[n % 16];
+        int rem = n % 16;
+        hexadecimal[i] = (rem < 10) ? (rem + '0') : (rem - 10 + ((uppercase) ? 'A' : 'a'));
         n /= 16;
         i++;
     }
@@ -80,6 +33,6 @@ int print_hexadecimal(unsigned int n, int uppercase)
         i--;
     }
 
-    return count;
+    return (count);
 }
 
