f3fda637baf266353ed74fbea180734c4881dd47
proabudu <abuduprosperyao@gmail.com>
2023-08-22 14:51:53 +0000

    fr

--- get_size.c
+++ get_size.c
@@ -7,6 +7,46 @@
  */
 int get_size(const char *format)
 {
-    // Implement length modifier handling logic...
+    int length_modifier = LENGTH_NONE; /*  Default value for no length modifier */
+
+    if (format == NULL)
+        return (LENGTH_NONE);
+
+    while (*format)
+    {
+        switch (*format)
+        {
+            case 'h':
+                if (*(format + 1) == 'h') /*  Check for 'hh' length modifier */
+                {
+                    length_modifier = LENGTH_HH;
+                    format += 2; /*  Move past 'hh' */ 
+                }
+                else
+                {
+                    length_modifier = LENGTH_H;
+                    format++; /*  Move past 'h' */
+                }
+                break;
+            case 'l':
+                if (*(format + 1) == 'l') /*  Check for 'll' length modifier */
+                {
+                    length_modifier = LENGTH_LL;
+                    format += 2; /*  Move past 'll' */
+                }
+                else
+                {
+                    length_modifier = LENGTH_L;
+                    format++; 
+                }
+                break;
+            default:
+
+                format++;
+                break;
+        }
+    }
+
+    return length_modifier;
 }
 
