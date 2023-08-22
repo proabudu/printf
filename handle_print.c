d1b9290545c7fef9e4e764b7c2cf7a06c1859dc0
proabudu <abuduprosperyao@gmail.com>
2023-08-22 21:49:54 +0000

    xdds

--- handle_print.c
+++ handle_print.c
@@ -8,7 +8,7 @@
  * @count: Pointer to count of characters printed
  * Return: Number of characters printed
  */
-int handle_print(const char *format, va_list args, int *count))
+int handle_print(const char *format, va_list args, int *count)
 {
     int printed = 0;
 
