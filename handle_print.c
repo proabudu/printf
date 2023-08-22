82518b51c6b0096cadee74ad4094e9b316e63b0d
proabudu <abuduprosperyao@gmail.com>
2023-08-22 21:56:15 +0000

    handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -5,6 +5,7 @@
 /**
  * handle_print - Handle the print conversion specifier
  * @format: Format string
+ * @args: Variable argument list
  * @count: Pointer to count of characters printed
  * Return: Number of characters printed
  */
@@ -18,16 +19,30 @@ int handle_print(const char *format, va_list args, int *count)
     switch (*format)
     {
         case 'c':
-            /* Handle %c conversion specifier */
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                printed++;
+            }
             break;
         case 's':
-            /* Handle %s conversion specifier */
+            {
+                char *str = va_arg(args, char *);
+                if (!str)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    printed++;
+                }
+            }
             break;
         case '%':
-            /* Handle %% conversion specifier */
+            write(1, "%", 1);
+            printed++;
             break;
         default:
-            /* Handle unknown specifier */
             write(1, "%", 1);
             write(1, format - 1, 1);
             *count += 2;
