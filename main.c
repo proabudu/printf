1385732fd5e0d3a6351cec84855f1353035c681f
proabudu <abuduprosperyao@gmail.com>
2023-08-22 23:03:00 -0700

    main.c updtate

--- main.c
+++ main.c
@@ -15,21 +15,4 @@ int main()
 
     return 0;
 }
-#include "main.h"
-
-int main()
-{
-    int count = 0;
-
-    count = _printf("Hello, %s!\n", "world");
-    _printf("Characters printed: %d\n", count);
-
-    count = _printf("Decimal: %d\n", 12345);
-    _printf("Characters printed: %d\n", count);
-
-    count = _printf("Hexadecimal: %x\n", 255);
-    _printf("Characters printed: %d\n", count);
-
-    return 0;
-}
 
